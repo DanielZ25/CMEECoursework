1335a6fe71e39267ec8b7cac08c3047de9169784
Daniel Zhu <hz1321@ic.ac.uk>
2025-10-17 03:29:42 +0100

    Moved all the .py files into week2 folder respectively

--- week2/code/debugme.py
+++ week2/code/debugme.py
@@ -4,8 +4,12 @@ def buggyfunc(x):
         try:
             y = y-1
             z = x/y
+        except ZeroDivisionError:
+            print(f"The result of dividing a number by zero is undefined")
         except:
             print(f"This didn't work;{x = }; {y = }")
+        else:
+            print(f"OK; {x = }, {z = };")
     return z
 
 buggyfunc(20)
